890b424a3832ccfca345b40b7107451a0a8910af
Dominik Adamek <Tassadar106@wp.pl>
2018-11-01 16:43:39 +0100

    1st

---
+++ routes/users.js
@@ -0,0 +1,38 @@
+// var express = require("express");
+// var router = express.Router();
+
+// /* GET users listing. */
+// router.get("/", function(req, res, next) {
+//   res.json([
+//     { id: 1, username: "somebody" },
+//     { id: 2, username: "somebody_else" },
+//     { id: 3, username: "Peter" }
+//   ]);
+// });
+
+// module.exports = router;
+const express = require("express");
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
+var router = express.Router();
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "englishwords"
+});
+
+const app = express();
+
+router.get("/", function(req, res) {
+  connection.query("SELECT * FROM words", function(err, result, fields) {
+    if (err) throw err;
+    res.send(JSON.stringify(result));
+  });
+});
+
+// app.listen(3001, () => {
+//   console.log("Go to http://localhost:3001/posts to see posts");
+// });
+module.exports = router;
